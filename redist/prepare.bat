3b3d027a7d1b7e3c611fe794d333966ea0100d07
sbenard <sbenard@mtwin.fr>
2017-12-05 10:52:00 +0100

    HL build fix Title fix

--- redist/prepare.bat
+++ redist/prepare.bat
@@ -1,4 +1,10 @@
 @echo off
+
+del CatsAreAssholes\hlboot.dat
+del *.zip
+del *.swf
+del *.js
+
 cd ..
 echo HL...
 haxe hl.hxml
@@ -13,7 +19,5 @@ echo Copying...
 copy bin\client.hl redist\CatsAreAssholes\hlboot.dat
 copy bin\client.swf redist
 copy bin\client.js redist
-copy index.html redist
 
-del redist\*.zip
 pause
